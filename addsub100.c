263e17fe33bdd5a94b12af74e114b39d213d0786
Jaroslav Beran <jaroslav.beran@gmail.com>
2017-11-20 21:31:53 +0100

    addsub100

--- addsub100.c
+++ addsub100.c
@@ -17,39 +17,40 @@ int do_add() {
         
         max = 10;
         result = 0;
-        for (i = 0; i < 10; i++) {
-                a = rand() % max;
-                b = rand() % max;
-                n1 = ((rand() % (max - 1)) + 1) * 10;
-                n2 = ((rand() % (max - 1)) + 1) * 10;
-                
-                
-                if (a > b) {
-                        a -= b;
-                } else {
-                        b -= a;
-                }
-                
-                if (n1 == n2) {
-                        n1 = n2 = 10;
-                } else {
-                        if (n1 > n2) {
-                                n1 -= n2;
-                        } else {
-                                n2 -= n1;
-                        }
-                }
-                
-                do {
-                        printf("%d + %d = ", a + n1, b + n2);
-                        c = get_user_input();
-                        if (c != (a + b + n1 + n2)) {
-                                printf("Spatne!\n");
-                                result++;
-                        }
-                } while (c != (a + b + n1 + n2));
+
+        a = rand() % max;
+        b = rand() % max;
+        n1 = ((rand() % (max - 1)) + 1) * 10;
+        n2 = ((rand() % (max - 1)) + 1) * 10;
+        
+        
+        if (a > b) {
+                a -= b;
+        } else {
+                b -= a;
         }
         
+        if (n1 == n2) {
+                n1 = n2 = 10;
+        } else {
+                if (n1 > n2) {
+                        n1 -= n2;
+                } else {
+                        n2 -= n1;
+                }
+        }
+        
+        do {
+                printf("%d + %d = ", a + n1, b + n2);
+                c = get_user_input();
+                if (c != (a + b + n1 + n2)) {
+                        printf("Spatne!\n");
+                        result++;
+                } else {
+                        printf("Spravne :-)\n");
+                }
+        } while (c != (a + b + n1 + n2));
+
         return result;
 }
 
@@ -59,52 +60,55 @@ int do_sub() {
         
         max = 10;
         result = 0;
-        for (i = 0; i < 10; i++) {
-                a = rand() % max;
-                b = rand() % max;
-                n1 = ((rand() % (max - 1)) + 1) * 10;
-                n2 = ((rand() % (max - 1)) + 1) * 10;
-                
-                if (a > b) {
-                        c = a;
-                } else {
-                        c = b;
-                        b = a;
-                }
-                
-                if (n1 > n2) {
-                        n3 = n1;
-                } else {
-                        n3 = n2;
-                        n2 = n1;
-                }
-                
-                do {
-                        printf("%d - %d = ", c + n3, b + n2);
-                        a = get_user_input();
-                        if (a != ((c +n3) - (b + n2))) {
-                                printf("Spatne!\n");
-                                result++;
-                        } 
-                } while (a != ((c +n3) - (b + n2)));
+
+        a = rand() % max;
+        b = rand() % max;
+        n1 = ((rand() % (max - 1)) + 1) * 10;
+        n2 = ((rand() % (max - 1)) + 1) * 10;
+        
+        if (a > b) {
+                c = a;
+        } else {
+                c = b;
+                b = a;
         }
+        
+        if (n1 > n2) {
+                n3 = n1;
+        } else {
+                n3 = n2;
+                n2 = n1;
+        }
+        
+        do {
+                printf("%d - %d = ", c + n3, b + n2);
+                a = get_user_input();
+                if (a != ((c +n3) - (b + n2))) {
+                        printf("Spatne!\n");
+                        result++;
+               } else {
+                        printf("Spravne :-)\n");
+                } 
+        } while (a != ((c +n3) - (b + n2)));
+
         
         return result;
 }
 
 int main(void) {
-        int res;
+int i, wrong;
         
+        wrong = 0;
         srand(time(NULL));
         
-        res = do_add();
-        printf("Pocet chyb: %d\n\n", res);
-        
-        res = do_sub();
-        printf("Pocet chyb: %d\n\n", res);
-        
-        printf("\nStiskni klavesu...\n");
-        getchar();
+        for (i = 0; i < 100; i++) {
+                if (rand() % 2) {
+                        wrong += do_add();
+                } else {
+                        wrong += do_sub();
+                }
+                printf("Celkem = %d, Chyb = %d\n\n", i + 1, wrong);
+        }
         
         return 0;
 }
